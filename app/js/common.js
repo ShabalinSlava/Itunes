41ac3e05e55d053b04fdaa6bdb7e322ddbcc3bb3
Shabalin Slava <svaznoy0309@gmail.com>
2020-07-29 11:02:29 +0800

    added active button

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
-$(function() {
+import { videoPlayerInit } from './videoPlayer.js'
+import { radioPlayerInit } from './radioPlayer.js'
+import { musicPlayerInit } from './musicPlayer.js'
 
-	// Custom JS
+const playerBtn = document.querySelectorAll('.player-btn')
+const playerBlock = document.querySelectorAll('.player-block')
+const temp = document.querySelector('.temp')
 
-});
+// Функции
+const deactivationPlayer = () => {
+	temp.style.display = 'none'
+	playerBtn.forEach((item) => {
+		item.classList.remove('active')
+	})
+	playerBlock.forEach((item) => {
+		item.classList.remove('active')
+	})
+}
+
+// Методы
+playerBtn.forEach((btn, i) => {
+	btn.addEventListener('click', () => {
+		deactivationPlayer()
+		btn.classList.add('active')
+		playerBlock[i].classList.add('active')
+	})
+})
+
+// Вызовы функций
+videoPlayerInit()
+radioPlayerInit()
+musicPlayerInit()
